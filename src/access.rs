2dfdbf2498323e9edde26edcc4461b0f6679ef67
MatthSy <matthieu.symphorien@edu.ece.fr>
2024-05-14 23:51:40 +0200

    Commit

---
+++ src/access.rs
@@ -0,0 +1,13 @@
+use std::io::{stdin};
+use std::str::from_utf8;
+use crate::my_hasher::hash;
+
+pub(crate) fn get_mdp_input() -> String {
+    println!("Entre le mdp grand fou");
+    let mut buffer = String::new();
+    let stdin = stdin();
+    stdin.read_line(&mut buffer).unwrap();
+
+    // Ligne dégueu:
+    from_utf8(&*hash(&buffer).to_vec()).unwrap().to_string() // hash le mdp et le passe en String
+}
